0e143344c6c3f438b510492266cc21f46b283b46
Machado <elitonmahcadod200@gmail.com>
2020-06-25 20:33:18 -0300

    gitignore

--- src/sample/AFD.java
+++ src/sample/AFD.java
@@ -1,20 +1,19 @@
 package sample;
 
-import guru.nidi.graphviz.attribute.Color;
 import guru.nidi.graphviz.attribute.Label;
-import guru.nidi.graphviz.attribute.Rank;
 import guru.nidi.graphviz.attribute.Shape;
 import guru.nidi.graphviz.engine.Format;
 import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.model.Graph;
 import guru.nidi.graphviz.model.Node;
-import static guru.nidi.graphviz.model.Factory.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
+
+import static guru.nidi.graphviz.model.Factory.*;
 
 public class AFD {
 
